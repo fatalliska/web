1273df808e80ccbeba1e8c0cc88c63d76e7a71dc
fatalliska <62687203+fatalliska@users.noreply.github.com>
2022-03-23 17:02:48 +0300

    homework6

--- node.js
+++ node.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require("mongodb");
-var server = require('http').createServer(app)
+const request = require('request');
+//var server = require.apply('http').createServer('app')
+var server = require('http').createServer(app);
+const rp = require('request-promise');
+const cheerio = require('cheerio')
 app.use(
     express.urlencoded({
        extended:true
@@ -13,80 +17,91 @@ app.get('/', function(req, res) {
     res.sendFile('/home/alisa/WebstormProjects/scoring/public/index.html')
 });
 app.post('/scoring', function(req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    // const client = new MongoClient('mongodb://127.0.0.1:27017');
     
-    client.connect(function(err, client) {
-        const db = client.db('test');
-        const collection = db.collection("users");
-        collection.find({email: req.body.email}).toArray(function(err, results){
-            if (results.length > 0) {
-                res.send("Такой email уже есть");
-                client.close();
-            } else {
-                collection.insertOne(req.body, function(err, result){
-                    if(err){ 
-                        return console.log(err);
-                }
-                console.log("inserted");
-                client.close();
-                var scoring=0;
-    //console.log(req.body);
-    let age = String(req.body.birthDate);
-    age = Number(age[0] + age[1] + age[2] + age[3]); 
-    let currentDate = new Date();
-    //let years = Math.round((currentDate - age)/(60 * 60 * 24 *1000 * 365)); 
-    let years = currentDate.getFullYear() - age;
-    //console.log(years);
-    if ((years > 60) || (years < 18)) {
-        res.send('У вас неподходящий возраст');
-        return;
-    } else if (years > 22) {
-        scoring = scoring + 0.3;
-    } else if (years > 20) {
-        scoring = scoring + (years-20)*0.1;
+    // client.connect(function(err, client) {
+    //     const db = client.db('test');
+    //     const collection = db.collection("users");
+    //     collection.find({email: req.body.email}).toArray(function(err, results){
+    //         if (results.length > 0) {
+    //             res.send("Такой email уже есть");
+    //             client.close();
+    //         } else {
+    //             collection.insertOne(req.body, function(err, result){
+    //                 if(err){ 
+    //                     return console.log(err);
+    //             }
+    //             console.log("inserted");
+    //             client.close();
+    // request('http://0.0.0.0:8085/hello', (err, response, body) => {
+    //     console.log(body)
+    // });
+    var options = {
+        method: 'post',
+        uri: 'http://0.0.0.0:8085/python',
+        body: req.body,
+        json: true
     }
-    let gender = String(req.body.gender);
-    if (gender=="female") {
-        scoring = scoring + 0.4;
-    }
-    let periodLife = Number(req.body.periodLife);
-    if (periodLife > 9) {
-        scoring = scoring + 0.42;
-    } else {
-        scoring = scoring + (0.042 * periodLife);
-    }
-    let profession = String(req.body.profession);
-    if (profession=="low") {
-        scoring = scoring + 0.55;
-    }
-    else if (profession=="other") {
-        scoring = scoring + 0.16;
-    }
-    let sphere = String(req.body.sphere);
-    if (sphere=="public") {
-        scoring = scoring + 0.21;
-    }
-    scoring = scoring + 0.059*Number(req.body.periodWork);
-    if (req.body.account) {
-        scoring = scoring + 0.45;
-    }
-    if (req.body.estate) {
-        scoring = scoring + 0.35;
-    }
-    if (req.body.insurance) {
-        scoring = scoring + 0.19;
-    }
-    if (scoring < 1.5) {
-        res.send("Ваш балл меньше 1.5, вам не одобрен кредит. Ваш балл: "+String(scoring));
-    } else {
-        res.send("Ваш балл больше 1.5, вам одобрен кредит. Ваш балл: "+String(scoring)); }
+    rp(options).then(function(parsedBody) {
+        res.send(parsedBody);
+    }).catch(function(err) {
+        console.log(err);
+    });
+    // var scoring=0;
+    // let age = String(req.body.birthDate);
+    // age = Number(age[0] + age[1] + age[2] + age[3]); 
+    // let currentDate = new Date();
+    // let years = currentDate.getFullYear() - age;
+    // if ((years > 60) || (years < 18)) {
+    //     res.send('У вас неподходящий возраст');
+    //     return;
+    // } else if (years > 22) {
+    //     scoring = scoring + 0.3;
+    // } else if (years > 20) {
+    //     scoring = scoring + (years-20)*0.1;
+    // }
+    // let gender = String(req.body.gender);
+    // if (gender=="female") {
+    //     scoring = scoring + 0.4;
+    // }
+    // let periodLife = Number(req.body.periodLife);
+    // if (periodLife > 9) {
+    //     scoring = scoring + 0.42;
+    // } else {
+    //     scoring = scoring + (0.042 * periodLife);
+    // }
+    // let profession = String(req.body.profession);
+    // if (profession=="low") {
+    //     scoring = scoring + 0.55;
+    // }
+    // else if (profession=="other") {
+    //     scoring = scoring + 0.16;
+    // }
+    // let sphere = String(req.body.sphere);
+    // if (sphere=="public") {
+    //     scoring = scoring + 0.21;
+    // }
+    // scoring = scoring + 0.059*Number(req.body.periodWork);
+    // if (req.body.account) {
+    //     scoring = scoring + 0.45;
+    // }
+    // if (req.body.estate) {
+    //     scoring = scoring + 0.35;
+    // }
+    // if (req.body.insurance) {
+    //     scoring = scoring + 0.19;
+    // }
+    // if (scoring < 1.5) {
+    //     res.send("Ваш балл меньше 1.5, вам не одобрен кредит. Ваш балл: "+String(scoring));
+    // } else {
+    //     res.send("Ваш балл больше 1.5, вам одобрен кредит. Ваш балл: "+String(scoring)); }
 
         });
-            }
-        });
-    });
+//             }
+//         });
+//     });
     
     
-});
+// });
 
 server.listen(3000);
